1959b8ff0dae645226b86d782dc5493b7a25bac3
Terry Young <terryyounghk@gmail.com>
2024-07-10 00:28:22 +0800

    refactor: prep for other boards/shields

--- config/common/layout_agnostic/keys.h
+++ config/common/layout_agnostic/keys.h
@@ -1,65 +1,11 @@
-/*
-  Key Positions
-    Side:               L: Left                R: Right
-    Columns:       5   4   3   2   1        1   2   3   4   5
-                ╭─────────────────────╮  ╭─────────────────────╮
-    U: Upper    │  0   1   2   3   4  │  │   5   6   7   8   9 │
-    H: Home     │ 10  11  12  13  14  │  │  15  16  17  18  19 │
-    L: Lower    │ 20  21  22  23  24  │  │  25  26  27  28  29 │
-    T: Thumb    ╰───────╮ 30  31  32  │  │  33  34  35 ╭───────╯
-                        ╰─────────────╯  ╰─────────────╯
+// This file contains keys or shortcut definitions that are independent from the keyboard layout
 
-  Notation
-    Side:               L: Left                R: Right
-    Columns:       5   4   3   2   1        1   2   3   4   5
-                ╭─────────────────────╮  ╭─────────────────────╮
-    U: Upper    │ LU5 LU4 LU3 LU2 LU1 │  │ RU1 RU2 RU3 RU4 RU5 │
-    H: Home     │ LH5 LH4 LH3 LH2 LH1 │  │ RH1 RH2 RH3 RH4 RH5 │
-    L: Lower    │ LL5 LL4 LL3 LL2 LL1 │  │ RL1 RL2 RL3 RL4 RL5 │
-    T: Thumb    ╰───────╮ LT3 LT2 LT1 │  │ RT1 RT2 RT3 ╭───────╯
-                        ╰─────────────╯  ╰─────────────╯
-*/
-
-// key positions
-#define LU5 0
-#define LU4 1
-#define LU3 2
-#define LU2 3
-#define LU1 4
-#define RU1 5
-#define RU2 6
-#define RU3 7
-#define RU4 8
-#define RU5 9
-
-#define LH5 10
-#define LH4 11
-#define LH3 12
-#define LH2 13
-#define LH1 14
-#define RH1 15
-#define RH2 16
-#define RH3 17
-#define RH4 18
-#define RH5 19
-
-#define LL5 20
-#define LL4 21
-#define LL3 22
-#define LL2 23
-#define LL1 24
-#define RL1 25
-#define RL2 26
-#define RL3 27
-#define RL4 28
-#define RL5 29
-
-#define LT3 30
-#define LT2 31
-#define LT1 32
-#define RT1 33
-#define RT2 34
-#define RT3 35
+// layer indices - used in layer switching in keymap
+#define QWERTY   0
+#define NUMBERS  1
+#define CODING   2
+#define CONTROLS 3
+#define AFK      4
 
 // behaviors
 #define MT(layer)                       &mo_tog layer layer   // Macro to apply momentary-layer-on-hold/toggle-layer-on-tap to a specific layer
